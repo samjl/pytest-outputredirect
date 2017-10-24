4462df96a1bbed555b9546d4b6566382730f8c40
Sam Lea <sam.lea@aviatnet.com>
2017-10-24 15:53:58 +1300

    Add logging to file in JSON format, add plugin config file

--- pytest_outputredirect/pytest_outputredirect.py
+++ pytest_outputredirect/pytest_outputredirect.py
@@ -9,25 +9,121 @@
 #         2. pytest output that fills console width stretches to 2
 #            lines after the addition on log level etc.
 
+import ConfigParser
+import json
+import os
+import pkg_resources
 import pytest
 import re
 import sys
 from collections import OrderedDict
 
 
+class DebugFunctionality:
+    def __init__(self, name, enabled):
+        self.name = name
+        self.enabled = enabled
+
+DEBUG = {"output-redirect": DebugFunctionality("redirect", True)}
+
+
 def pytest_addoption(parser):
     # Add disable redirection command line option.
     parser.addoption("--no-redirect", action="store_true",
-                     help="Disable output redirection (disables plugin)")
+                     help="Disable output redirection (disables plugin). "
+                          "Overrides value set in config.cfg")
+    parser.addoption('--root-dir', type=str, default=None,
+                     help='Full path to local base directory to save test '
+                          'logs to. Overrides value set in config.cfg')
+    parser.addoption("--no-json", action="store_true",
+                     help="Don't save log to JSON file (std out only)")
 
 
 def pytest_configure(config):
-    # Process plugin command line arguments. Enable/disable redirection.
+    # Load user defined configuration from file
+    config_path = pkg_resources.resource_filename('pytest_outputredirect', '')
+    parser = ConfigParser.ConfigParser()
+    parser.read(os.path.join(config_path, "config.cfg"))
+
+    for functionality in DEBUG.keys():
+        try:
+            DEBUG[functionality].enabled = parser.getboolean("debug",
+                                                             functionality)
+        except Exception as e:
+            print e
+
+    if config.getoption("--no-json"):
+        LogOutputRedirection.json_log = False
+        _debug_print("JSON logging is disabled (command line)",
+                     DEBUG["output-redirect"])
+    else:
+        LogOutputRedirection.json_log = not(parser.getboolean("general",
+                                                              "no-json"))
+        _debug_print("JSON logging is {}abled (config.cfg)"
+                     .format("en" if LogOutputRedirection.json_log else "dis"),
+                     DEBUG["output-redirect"])
+
     if not (config.getoption("--no-redirect") or
             isinstance(sys.stdout, LogOutputRedirection)):
         log_redirect = LogOutputRedirection()
         sys.stderr = log_redirect
         sys.stdout = log_redirect
+
+    if LogOutputRedirection.json_log:
+        root_dir = config.getoption("--root-dir")
+        if not root_dir:
+            root_dir = parser.get("general", "root-dir")
+            _debug_print("Using root directory {} (config.cfg)"
+                         .format(root_dir), DEBUG["output-redirect"])
+        else:
+            _debug_print("Using root directory '{}' (command line)"
+                         .format(root_dir), DEBUG["output-redirect"])
+        LogOutputRedirection.root_directory = root_dir
+        # Create root logs directory if required
+        if not os.path.exists(LogOutputRedirection.root_directory):
+            _debug_print("Creating directories {}".format(LogOutputRedirection.
+                                                          root_directory),
+                         DEBUG["output-redirect"])
+            os.makedirs(LogOutputRedirection.root_directory)
+
+        open(os.path.join(LogOutputRedirection.root_directory, "session.json"),
+             'w').close()
+        LogOutputRedirection.session_file_path = os.path.join(
+            LogOutputRedirection.root_directory, "session.json")
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_setup(item):
+    _debug_print("Creating log file for module {}, test function {}".format(
+        item.module.__name__, item.name), DEBUG["output-redirect"])
+
+    if LogOutputRedirection.json_log:
+        # Create module dir if required and test function dir within
+        # this and then module_function.json file
+        path_to_test_dir = os.path.join(LogOutputRedirection.root_directory,
+                                        item.module.__name__, item.name)
+        _debug_print("Path to test directory: {}".format(path_to_test_dir),
+                     DEBUG["output-redirect"])
+        if not os.path.exists(path_to_test_dir):
+            _debug_print("Creating directories", DEBUG["output-redirect"])
+            os.makedirs(path_to_test_dir)
+
+        open(os.path.join(path_to_test_dir, "log.json"), 'w').close()
+        LogOutputRedirection.test_file_path = os.path.join(path_to_test_dir,
+                                                           "log.json")
+        _debug_print("Path to json log file: {}".format(
+            LogOutputRedirection.test_file_path), DEBUG["output-redirect"])
+
+    yield
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_report_teststatus(report):
+    yield
+    if report.when == "teardown":
+        _debug_print("OUTPUTREDIRECT LAST TEARDOWN MESSAGE",
+                     DEBUG["output-redirect"])
+        LogOutputRedirection.test_file_path = None
 
 
 def _is_start_or_end(msg):
@@ -41,6 +137,11 @@ class LogOutputRedirection:
     # Output redirection class. Redirects sys.stdout and stderr to
     # write method below.
     messageIndex = 0
+    json_log = None
+    # json log file paths
+    root_directory = None
+    session_file_path = None  # created at plugin configuration stage
+    test_file_path = None  # file is created in setup phase
 
     def __init__(self):
         self.printStdout = sys.stdout
@@ -109,3 +210,37 @@ class LogOutputRedirection:
         self.printStdout.write("{0[level]}-{0[step]} [{0[index]}] {0[text]}\n"
                                .format(log_entry))
         self.printStdout.flush()
+
+        # Complete session json log file.
+        # Ensure the file always contains valid JSON.
+        if LogOutputRedirection.json_log and \
+                LogOutputRedirection.session_file_path:
+            if os.stat(LogOutputRedirection.session_file_path).st_size != 0:
+                with open(LogOutputRedirection.session_file_path, "rb+") as f:
+                    f.seek(-2, os.SEEK_END)
+                    f.write(",\n")
+            with open(LogOutputRedirection.session_file_path, "a") as f:
+                if os.stat(LogOutputRedirection.session_file_path).st_size == 0:
+                    f.write("[")
+                json.dump(log_entry, f, separators=(",", ":"))
+                f.write("]\n")
+
+        # Test function specific json log file. Contains setup, call
+        # and teardown.
+        if LogOutputRedirection.json_log and \
+                LogOutputRedirection.test_file_path:
+            if os.stat(LogOutputRedirection.test_file_path).st_size != 0:
+                with open(LogOutputRedirection.test_file_path, "rb+") as f:
+                    f.seek(-2, os.SEEK_END)
+                    f.write(",\n")
+            with open(LogOutputRedirection.test_file_path, "a") as f:
+                if os.stat(LogOutputRedirection.test_file_path).st_size == 0:
+                    f.write("[")
+                json.dump(log_entry, f, separators=(",", ":"))
+                f.write("]\n")
+
+
+def _debug_print(msg, flag):
+    # Print a debug message if the corresponding flag is set.
+    if flag.enabled:
+        print "DEBUG({}): {}".format(flag.name, msg)
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Sam Lea',
     author_email='samjlea@gmail.com',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=['pytest>=2.8.0', "pytest-loglevels>=0.2.0"],
     # the following makes a plugin available to pytest
     entry_points={'pytest11': ['outputredirect = pytest_outputredirect.pytest_outputredirect']},
