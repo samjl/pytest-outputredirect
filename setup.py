a4496679b3babd4962491a6a34952634a658f30a
Sam Lea <sam.lea@aviatnet.com>
2017-10-24 13:06:37 +1300

    Create package diretory and move files

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pytest-outputredirect",
     version='0.1.0',
     author='Sam Lea',
     author_email='samjlea@gmail.com',
-    py_modules=['pytest_outputredirect'],
+    packages=find_packages(),
     install_requires=['pytest>=2.8.0', "pytest-loglevels>=0.2.0"],
     # the following makes a plugin available to pytest
-    entry_points={'pytest11': ['outputredirect = pytest_outputredirect']},
+    entry_points={'pytest11': ['outputredirect = pytest_outputredirect.pytest_outputredirect']},
     # custom PyPI classifier for pytest plugins
     classifiers=["Framework :: Pytest"],
 )
